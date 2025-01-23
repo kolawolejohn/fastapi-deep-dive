f810af6be02c6727df4bbae02187915775a59870
kolawolejohn <kolawole.oajohn@gmail.com>
2025-01-24 00:34:46 +0100

    Basic CRUD operations with fastapi

---
+++ main.py
@@ -0,0 +1,57 @@
+from typing import Optional, List
+from fastapi import FastAPI, HTTPException, Header, status
+import uvicorn
+from loc_db import books
+from models.book_model import Book, BookUpdateModel
+
+app = FastAPI()
+
+
+@app.get("/books", status_code=200, response_model=List[Book])
+async def get_all_books():
+    return books
+
+
+@app.post("/books", status_code=status.HTTP_201_CREATED)
+async def create_book(data: Book) -> dict:
+    new_book = data.model_dump()
+    books.append(new_book)
+    return new_book
+
+
+@app.get("/books/{id}", status_code=status.HTTP_200_OK)
+async def get_book(id: int) -> dict:
+    for book in books:
+        if book["id"] == id:
+            return book
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Record not found"
+    )
+
+
+@app.patch("/books/{id}", status_code=status.HTTP_200_OK)
+async def update_book(id: int, data: BookUpdateModel) -> dict:
+    for book in books:
+        if book["id"] == id:
+            book["title"] = data.title
+            book["publisher"] = data.publisher
+            book["page_count"] = data.page_count
+            return book
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Record not found"
+    )
+
+
+@app.delete("/books/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(id: int):
+    for book in books:
+        if book["id"] == id:
+            books.remove(book)
+            return
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Record not found"
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
---
+++ models/book_model.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str | None = None
+    publisher: str
+    page_count: int
+    language: str | None = None
