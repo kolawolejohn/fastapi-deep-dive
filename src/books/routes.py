000d9e529ad2e5c5d2a79c6f3ae9d114ff6db0fa
kolawolejohn <kolawole.oajohn@gmail.com>
2025-01-24 01:38:11 +0100

    Organise the code into a better folder structure

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,25 +1,24 @@
+from fastapi import APIRouter, HTTPException, status
 from typing import List
-from fastapi import FastAPI, HTTPException, status
-import uvicorn
-from loc_db import books
-from models.book_model import Book, BookUpdateModel
+from src.books.book_data import books
+from src.books.schemas import Book, BookUpdateModel
 
-app = FastAPI()
+book_router = APIRouter()
 
 
-@app.get("/books", status_code=200, response_model=List[Book])
+@book_router.get("/", status_code=200, response_model=List[Book])
 async def get_all_books():
     return books
 
 
-@app.post("/books", status_code=status.HTTP_201_CREATED)
+@book_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(data: Book) -> dict:
     new_book = data.model_dump()
-    books.append(new_book)
+    books.book_routerend(new_book)
     return new_book
 
 
-@app.get("/books/{id}", status_code=status.HTTP_200_OK)
+@book_router.get("/{id}", status_code=status.HTTP_200_OK)
 async def get_book(id: int) -> dict:
     for book in books:
         if book["id"] == id:
@@ -29,7 +28,7 @@ async def get_book(id: int) -> dict:
     )
 
 
-@app.patch("/books/{id}", status_code=status.HTTP_200_OK)
+@book_router.patch("/{id}", status_code=status.HTTP_200_OK)
 async def update_book(id: int, data: BookUpdateModel) -> dict:
     for book in books:
         if book["id"] == id:
@@ -42,7 +41,7 @@ async def update_book(id: int, data: BookUpdateModel) -> dict:
     )
 
 
-@app.delete("/books/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@book_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(id: int):
     for book in books:
         if book["id"] == id:
@@ -51,7 +50,3 @@ async def delete_book(id: int):
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail="Record not found"
     )
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
